f3b531725034f2adf584a7fb022303c51699529d
= <=>
2020-10-26 19:24:32 +0800

    adjustment

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.admin')
+@section('pagename')
+    Dashboard
+@endsection
+
+{{-- @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                        
+                    @if (Auth::user()->act_typ == 4)
+                        <span>You log as Super Admin {{Auth::user()->username}}</span>
+                    @elseif (Auth::user()->act_typ == 1)
+                        <span>You log as Super Agent {{Auth::user()->username}}</span>
+                    @elseif (Auth::user()->act_typ == 2)
+                        <span>You log as Agent {{Auth::user()->username}}</span>
+                    @elseif (Auth::user()->act_typ == 3)
+                        <span>You log as Player {{Auth::user()->username}}</span>
+                    @endif
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
+
+@section('content')
+    <dashboard-component></dashboard-component>
+@endsection
