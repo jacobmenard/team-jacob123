1fd94c89f9d80bb8d69e3307be4d83732ac28745
= <=>
2020-10-28 20:09:06 +0800

    added models

---
+++ app/TblAccPoints.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TblAccPoints extends Model
+{
+    protected $table = 'tbl_acc_points';
+    protected $primaryKey = 'pt_no';
+}
---
+++ app/TblTransactions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TblTransactions extends Model
+{
+    protected $table = 'tbl_transactions';
+    protected $primaryKey = 'trans_no';
+    
+}
